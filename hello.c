82b6ebe2750d8aa2edbb7b1d8a83a6164936f78f
Jitendra Bhatia <68694330+jitendrabbhatia@users.noreply.github.com>
2020-12-06 23:14:12 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,5 +6,7 @@ int a=5,b=6,c;
 printf("welcome to Prog Lab\n");
 /* here write a logic to swap two numbers
 */
+  
+//print ("swapping done, output should be a=6,b=5 ; your output is a=%d and b =%d",a,b)
 return 0;
 }
